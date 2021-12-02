e3e6b75f087b6a85005d4221e45400d4a041bea4
Веремеенко Владимир <vova-veremeenko2004@yandex.ru>
2021-12-02 11:38:36 +0300

    Добавьте файлы проекта.

---
+++ Notes/Notes/Views/AboutPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+
+            await Launcher.OpenAsync("https://aka.ms/xamarin-quickstart");
+        }
+    }
+}
---
+++ Notes/Notes/Views/NotesPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Notes.Models;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class NotesPage : ContentPage
+    {
+        public NotesPage()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        async void OnAddClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(NoteEntryPage));
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            collectionView.ItemsSource = await App.Database.GetNotesAsync();
+        }
+
+        async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection != null)
+            {
+                Note note = (Note)e.CurrentSelection.FirstOrDefault();
+                await Shell.Current.GoToAsync($"{nameof(NoteEntryPage)}?{nameof(NoteEntryPage.ItemId)}={note.ID.ToString()}");
+            }
+        }
+    }
+}
